509672ccffeac938686e4d75141439314e020b92
Steve Rayner <srayner02@gmail.com>
2016-01-14 22:20:46 +0000

    Added config and some text controller actions.

--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -13,6 +13,7 @@ use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
 
 use Zend\Http\Client;
+use ZendService\Twitter\Twitter;
 
 class IndexController extends AbstractActionController
 {
@@ -24,21 +25,42 @@ class IndexController extends AbstractActionController
     public function gitAction()
     {
         // Set the configuration parameters
-        $config = array(
-            'adapter'    => 'Zend\Http\Client\Adapter\Curl',
-            'proxy_host' => '172.16.20.2',
-            'proxy_port' => 8080,
-            'curloptions' => array(
-                CURLOPT_SSL_VERIFYPEER => false,
-                CURLOPT_SSL_VERIFYHOST => false,
-            ),
-        );
-        $client = new Client('https://api.github.com/users/srayner', $config);
+        $config = $this->getServiceLocator()->get('config')['http_client_config'];
+        $gitAccountName = $this->getServiceLocator()->get('config')['git_account_name'];
+        $client = new Client('https://api.github.com/users/' . $gitAccountName, $config);
         $response = $client->send();
         
-        echo $response->getStatusCode();
-        echo '<br>';
-        echo $response->getBody();
+        $git = json_decode($response->getBody());
+        echo $git->login . '<br>';
+        echo '<img height="64" src="' . $git->avatar_url . '">';
         die;
+    }
+    
+    public function eventsAction()
+    {
+        // Set the configuration parameters
+        $config = $this->getServiceLocator()->get('config')['http_client_config'];
+        $githubAccountName = $this->getServiceLocator()->get('config')['github_account_name'];
+        $githubFullName = $this->getServiceLocator()->get('config')['github_full_name'];
+        
+        $client = new Client('https://api.github.com/users/' . $gitAccountName . '/events/public', $config);
+        $response = $client->send();
+        
+        $git = json_decode($response->getBody());
+        foreach($git as $event) {
+            if ($event->type == 'PushEvent') {
+                $url = "https://github.com/" . $event->repo->name;
+                echo $githubFullName . ' pushed code to <a href="' . $url . '">' . $event->repo->name . '</a><br>';
+            }
+        }
+        die;
+    }
+    
+    public function tweetAction()
+    {
+        $config = $this->getServiceLocator()->get('config')['twitter'];
+        $twitter = new Twitter($config);
+        $twitter->statuses->update('Hello world!');
+        die('ok');
     }
 }
