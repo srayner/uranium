11f35ecbf60014d4297c55f5cd78e5e84aa52524
Steve Rayner <srayner02@gmail.com>
2016-01-21 20:40:06 +0000

    Twitter settings form now saves to local data file.

--- module/Application/src/Application/Controller/TwitterController.php
+++ module/Application/src/Application/Controller/TwitterController.php
@@ -10,6 +10,19 @@ class TwitterController extends AbstractActionController
     public function indexAction()
     {
         $form = $this->getServiceLocator()->get('twitter_form');
+        $request = $this->getRequest();
+        if($request->isPost()){
+            $form->setData($request->getPost());
+            if ($form->isValid())
+            {
+                $data = $form->getData();
+                unset($data['send']);
+                $service = $this->getServiceLocator()->get('settings_service');
+                $service->persist('twitter', $data);
+                return $this->redirect()->toRoute('home');
+            }
+        }
+        
         return new ViewModel(array(
             'form' => $form
         ));
